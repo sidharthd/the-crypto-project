825036cff4a9ee5c3790ee92d66158fc96873e10
Sidharth D <sidharth363@gmail.com>
2016-01-31 07:37:03 +0530

    DES-fiestel-expansion of right half to 48 bits

---
+++ des/__init__.py
@@ -0,0 +1,1 @@
+from fiestel import fiestel
---
+++ des/fiestel.py
@@ -0,0 +1,17 @@
+def fiestel(input_block, round_keys):
+	rounds = 16
+	length = len(input_block)
+	left_half = input_block[:length/2]
+	right_half = input_block[length/2:]
+	
+	for round in range(rounds):	
+		next_left_half = right_half
+		expanded_right_half = ''
+		for i in range(4, (length/2)+1, 4):
+			try:
+				expanded_right_half += right_half[i-4:i]
+				expanded_right_half += right_half[i] + right_half[i-1]
+			except IndexError:
+				pass
+		expanded_right_half = right_half[length/2-1] + expanded_right_half + right_half[0]
+		return expanded_right_half
